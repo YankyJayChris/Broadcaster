516cc0c41b05980c6381f6755fc253a18ab32733
IGIHOZO Jean Christian <igihozo35@gmail.com>
2019-11-06 10:58:53 +0200

    deleted docs folder it was not needed it was for test purpose

--- docs/store/index.js
+++
@@ -1,9 +0,0 @@
-import Store from "./Store.js";
-import { redflagReducer } from "./reducers/RedFlagReducer.js";
-
-
-const allReducers = {
-  redFlags: redflagReducer
-};
-
-export const store = new Store(allReducers);
