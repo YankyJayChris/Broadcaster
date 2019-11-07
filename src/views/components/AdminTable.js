d369757ec6336da2ceb27795bea5fae33e964555
IGIHOZO Jean Christian <igihozo35@gmail.com>
2019-11-07 13:21:21 +0200

    created table of admin working very well

---
+++ src/views/components/AdminTable.js
@@ -0,0 +1,32 @@
+let AdminTable = {
+  render: async posts => {
+    let view = posts
+      .map((post,index) => {
+          
+        return ` <tr>
+                    <td>${index+1}</td>
+                    <td>${post.title}</td>
+                    <td>REd-flag</td>
+                    <td>-1.9489, 30.1015</td>
+                    <td>
+                        <span class="dropdown">
+                            <select>
+                                <option>change staus</option>
+                                <option>Draft</option>  
+                                <option>Under investigation</option>  
+                                <option>Rejected</option>  
+                                <option>Resolved</option>  
+                            </select>
+                        </span>
+                    </td>
+                </tr>`;
+      })
+      .join("\n");
+
+    return view;
+  },
+  // All the code related to DOM interactions and controls
+  events: async () => {}
+};
+
+export default AdminTable;
--- src/views/pages/Admin.js
+++ src/views/pages/Admin.js
@@ -1,19 +1,53 @@
 import Sidebar from "../components/Sidebar.js";
 import Dashborad from "./../components/Dashborad.js";
+import AdminTable from './../components/AdminTable.js';
 
-let Feed = {
+
+// importing store and actions
+import {store} from "../../store/index.js";
+import { getRedFlag } from "../../store/actions/RedFlagAction.js";
+
+let posts;
+let Admin = {
   render: async () => {
-    let SidebarFeed = await Sidebar.render();
-    let DashboradFeed = await Dashborad.render();
-    return /*html*/ `
+    if (!posts) {
+      //   preventing the the getRedflag to be called when it have been called
+      await getRedFlag();
+      //   post must be undefined
+      console.log(posts);
+    }
+    //   subscribing to the store to get data when there is any changed
+    store.subscribe(newState => {
+      posts = newState.redFlags.data;
+    });
+
+    // getting some components to build the UI
+    let SidebarAdmin = await Sidebar.render();
+    let DashboradAdmin = await Dashborad.render();
+
+    let view = /*html*/ `
             <div class="admin-page">
-               ${SidebarFeed} 
+               ${SidebarAdmin} 
                <div class="admin-center">
-                    ${DashboradFeed}
+                    ${DashboradAdmin}
+                    <div class="card m-top">
+                    <table id="admin-table">
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Type</th>
+                        <th>Location</th>
+                        <th>View</th>
+                    </tr>
+                      ${await AdminTable.render(posts)}
+                    </table>
+                   </div> 
                </div>
 
             </div>
         `;
+
+    return view;
   },
   // All the code related to DOM interactions and controls
   events: async () => {
@@ -22,4 +56,4 @@ let Feed = {
   }
 };
 
-export default Feed;
+export default Admin;
